d14c47f230e2eda8e7d9c7c8b2cc3d43f317bf7c
IslamAlAbd <islam.abd.4321@gmail.com>
2026-06-18 09:59:28 +0300

    add needed moduls and courses schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,21 @@ import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import 'dotenv/config';
 import { UsersModule } from 'users/users.module';
+import { CoursesModule } from './courses/courses.module';
+import { CategoriesModule } from './categories/categories.module';
+import { GradesModule } from './grades/grades.module';
+import { EnrollmentModule } from './enrollment/enrollment.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot(
-      process.env.MONGODB_URI || 'mongodb://localhost:27017/mydatabase',
+      process.env.MONGODB_URI || 'mongodb://localhost:27017/training_db',
     ),
     UsersModule,
+    CoursesModule,
+    CategoriesModule,
+    GradesModule,
+    EnrollmentModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/courses/schemas/course.schema.ts
@@ -0,0 +1,25 @@
+import { Prop, Schema } from "@nestjs/mongoose";
+
+export type CourseDocument = Course & Document;
+
+@Schema({timestamps: true})
+export class Course {
+    @Prop({required: true, unique: true,ref: 'CourseCategory'})
+    category_id: number;
+    @Prop({required: true, unique: true,ref: 'Teachers'})
+    teacher_id: number;
+    @Prop({required: true})
+    title: string;
+    @Prop({required: true})
+    description: string;
+    @Prop({required: true})
+    price: number;
+    @Prop({required: true})
+    capacity: number;
+    @Prop({required: true})
+    start_date: Date;
+    @Prop({required: true})
+    end_date: Date;
+    @Prop({required: true, enum: ["available", "full", "completed","finished", "cancelled", "upcoming"]})
+    status: string;
+}
